4d044b9759102eb97f32b2f46e723881ac18bd23
Oleg Kishenkov <oleg_kishenkov@mail.ru>
2024-11-16 18:49:44 +0300

    add polls and tags to admin, change readme

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-auto-graphql", # Replace with your own username
-    version="0.0.6",
+    version="0.1.0",
     author="olegkishenkov",
     author_email="oleg.kishenkov@gmail.com",
     description="an automatic GraphQL API for all the models in a Django project",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/olegkishenkov/django-auto-graphql",
-    packages=setuptools.find_packages(where='auto_graphql'),
+    packages=setuptools.find_packages(include=('auto_graphql', )),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8.1',
+    install_requires=['graphene-django', ],
 )
